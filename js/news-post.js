ed95c586be8b68841b26469e78ebb5892a536806
soycoder <43911669+soycoder@users.noreply.github.com>
2021-01-10 03:27:44 +0700

    +

---
+++ js/news-post.js
@@ -0,0 +1,68 @@
+const listGallery = document.querySelector("#listGallery");
+
+const title = document.querySelector("title");
+const newsTitle = document.querySelector("#newsTitle");
+const timeAgo = document.querySelector("#timeAgo");
+const auther = document.querySelector("#auther");
+const description = document.querySelector("#description");
+
+const urlParams = new URLSearchParams(window.location.search);
+const id = urlParams.get("q");
+// console.log(id);
+
+function timeSince(date) {
+  var seconds = Math.floor((new Date() - date) / 1000);
+
+  var interval = seconds / 31536000;
+
+  if (interval > 1) {
+    return Math.floor(interval) + " years";
+  }
+  interval = seconds / 2592000;
+  if (interval > 1) {
+    return Math.floor(interval) + " months";
+  }
+  interval = seconds / 86400;
+  if (interval > 1) {
+    return Math.floor(interval) + " days";
+  }
+  interval = seconds / 3600;
+  if (interval > 1) {
+    return Math.floor(interval) + " hours";
+  }
+  interval = seconds / 60;
+  if (interval > 1) {
+    return Math.floor(interval) + " minutes";
+  }
+  return Math.floor(seconds) + " seconds";
+}
+
+// News
+fetch("../data_json/news.json")
+  .then((response) => response.json())
+  .then((data) => {
+    let items = data.items;
+    items.forEach((element) => {
+      // console.log(element);
+      if (element.id_name === id) {
+        title.innerHTML = element.title;
+        newsTitle.innerHTML += element.title;
+        timeAgo.innerHTML = timeSince(element.datetime);
+        auther.innerHTML = element.auther;
+        description.innerHTML = element.description;
+        element.gallery.forEach((p) => {
+          let node = `
+            <div class="cell-6"><a class="thumbnail-default"
+                    href="images/portfolio/gallery-10-1200x800-original.jpg"
+                    data-lightgallery="item">
+                    <img src="images/portfolio/gallery-10-320x320.jpg" alt="" width="320"
+                        height="320">
+                    <span class="icon novi-icon fa-search-plus"></span>
+                </a>
+            </div>`;
+            listGallery.innerHTML += node;
+          
+        });
+      }
+    });
+  });
--- js/student_council.js
+++ js/student_council.js
@@ -1,10 +1,10 @@
-const listLecturer = document.querySelector("#listLecturer");
-const listStaff = document.querySelector("#listStaff");
+const head = document.querySelector("#head");
+const listMembers = document.querySelector("#members");
 const loader1 = document.querySelector("#loader1");
-const loader2 = document.querySelector("#loader2");
 
-// Lecturers
-fetch("../data_json/lecturers.json")
+
+// Head
+fetch("../data_json/student_council.json")
   .then((response) => response.json())
   .then((data) => {
     let items = data.items;
@@ -14,21 +14,23 @@ fetch("../data_json/lecturers.json")
       let node = `
       <div class="cell-sm-6 cell-md-3">
         <img class="img-responsive reveal-inline-block img-rounded"
-        src="../images/users/Lecturer-${element.id_name}.jpg" width="270" height="270" alt="">
+        src="../images/users/student_council-${element.id_name}.jpg" width="270" height="270" alt="">
         <div class="offset-top-20">
             <h6 class="text-bold text-primary">${element.name_th}</h6>
         </div>
         <div class="offset-top-5">
-            <p>${element.department_th}</p>
+            <p>${element.position}</p>
         </div>
     </div>`;
-      listLecturer.innerHTML += node;
+      if (element.position === "ประธาน" || element.position === "รองประธาน") {
+        head.innerHTML += node;
+      }
     });
     loader1.classList.remove("active");
   });
 
-// staffs
-fetch("../data_json/staffs.json")
+// member
+fetch("../data_json/student_council.json")
   .then((response) => response.json())
   .then((data) => {
     let items = data.items;
@@ -38,15 +40,16 @@ fetch("../data_json/staffs.json")
       let node = `
         <div class="cell-sm-6 cell-md-3">
           <img class="img-responsive reveal-inline-block img-rounded"
-          src="../images/users/Staff-${element.id_name}.jpg" width="270" height="270" alt="">
+          src="../images/users/student_council-${element.id_name}.jpg" width="270" height="270" alt="">
           <div class="offset-top-20">
               <h6 class="text-bold text-primary">${element.name_th}</h6>
           </div>
           <div class="offset-top-5">
-              <p>${element.job}</p>
+              <p>${element.position}</p>
           </div>
       </div>`;
-      listStaff.innerHTML += node;
+      if (element.position !== "ประธาน" && element.position !== "รองประธาน") {
+        listMembers.innerHTML += node;
+      }
     });
-    loader2.classList.remove("active");
   });
